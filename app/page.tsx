a2423bfbb06b83f0de626a7050c3a690ff6c1c4b
MikRyam <mikhailryamov@gmail.com>
2025-01-12 00:00:24 +0300

    Add a theme toggler

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-// import ThemeSwitcher from "@/components/ThemeSwitcher";
-
 const Home = () => {
   return (
     <>
       <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-	  {/* <ThemeSwitcher /> */}
     </>
   );
 };
--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
 
 // import Theme from "./Theme";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
@@ -19,10 +20,10 @@ const Navbar = () => {
         </p>
       </Link>
 
-	  <p>Global Search</p>
+      <p>Global Search</p>
 
       <div className="flex-between gap-5">
-        Theme
+        <Theme />
       </div>
     </nav>
   );
